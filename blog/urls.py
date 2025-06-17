4f89adedb97b8a8014f332cf045abb1cb7729361
Davi Silva de Deus <davisilvadedeus18@gmail.com>
2025-06-16 23:08:02 -0300

    Adicionando rotas no urls.py para conectar as novas views

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('', views.PostView.as_view(), name='home')
+    path("", views.PostView.as_view(), name="home"),
+    path("posts/", views.PostListView.as_view(), name="post_list"),
+    path("posts/<int:pk>/", views.PostDetailView.as_view(), name="post_detail"),
 ]
-
