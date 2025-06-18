5e0b95d521d0b425863250fe32738e1b5f9c28d1
Davi Silva de Deus <davisilvadedeus18@gmail.com>
2025-06-18 20:52:38 -0300

    criando as nova views PostListViews e postDetailView

---
+++ blog/views/post_view.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.views import generic
+from blog.models import Post  
+
+
+class PostView(generic.View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("Hello World!")
+
+
+class PostListView(generic.ListView):
+    model = Post
+    template_name = "post_list.html"
+    context_object_name = "posts"
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = "post_detail.html"
+    context_object_name = "post"
