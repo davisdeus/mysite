4144eca8fec5f32f8033e34eb564939da27e6ecb
Davi Silva de Deus <davisilvadedeus18@gmail.com>
2025-06-19 00:53:03 -0300

    feat(admin): configurar exibição e filtros para o modelo Post

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "status", "slug", "created_on")
+    list_filter = ("status",)
+    search_fields = ["title", "content"]
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Post, PostAdmin)
