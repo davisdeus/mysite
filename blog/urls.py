0cc1a999215821031d2ac1b5824ab218b3d35fec
Davi Silva de Deus <davisilvadedeus18@gmail.com>
2025-06-25 11:22:37 -0300

    Exclui rotas posts/ e posts/<pk>/ para centralizar navegação na home

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,4 @@ from blog import views
 
 urlpatterns = [
     path("", views.PostView.as_view(), name="home"),
-    path("posts/", views.PostListView.as_view(), name="post_list"),
-    path("posts/<int:pk>/", views.PostDetailView.as_view(), name="post_detail"),
 ]
