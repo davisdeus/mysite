f93b58ccda2c60a4aba6cc54b7d459ceabd78edd
Davi Silva de Deus <davisilvadedeus18@gmail.com>
2025-06-25 11:25:50 -0300

    Simplifica views: remove ListView e DetailView para focar em PostView com Hello World

--- blog/views/post_view.py
+++ blog/views/post_view.py
@@ -6,15 +6,3 @@ from blog.models import Post
 class PostView(generic.View):
     def get(self, request, *args, **kwargs):
         return HttpResponse("Hello World!")
-
-
-class PostListView(generic.ListView):
-    model = Post
-    template_name = "post_list.html"
-    context_object_name = "posts"
-
-
-class PostDetailView(generic.DetailView):
-    model = Post
-    template_name = "post_detail.html"
-    context_object_name = "post"
